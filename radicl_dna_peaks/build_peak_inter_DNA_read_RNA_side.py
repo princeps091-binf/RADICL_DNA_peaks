5fa2e06835a3044c65ce69c43d8d90b06121fc20
princeps091-binf <princeps091@gmail.com>
2023-03-09 18:25:30 +0100

    subset RNA-side of peak intersecting reads

---
+++ radicl_dna_peaks/build_peak_inter_DNA_read_RNA_side.py
@@ -0,0 +1,19 @@
+#%%
+import bioframe as bf
+import numpy as np
+import altair as alta
+import subprocess
+import pandas as pd
+#%%
+rna_radicl_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/raw/RNA/RADICL_iPSC_RNA.bed"
+peak_read_tbl_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA_peak_analysis/peak_DNA_read_inter_tbl.tsv"
+
+out_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA_peak_analysis/RADICL_peak_RNA.bed"
+
+#%%
+peak_read_inter_tbl = pd.read_csv(peak_read_tbl_file,delimiter="\t")
+#%%
+iter_csv = pd.read_csv(rna_radicl_file,sep="\t",header=None, iterator=True, chunksize=100_000)
+df = pd.concat([chunk.loc[chunk.iloc[:,3].isin(peak_read_inter_tbl.read_ID.to_list()),:] for chunk in iter_csv])
+
+# %%
--- radicl_dna_peaks/build_peak_reads_map.py
+++ radicl_dna_peaks/build_peak_reads_map.py
@@ -8,7 +8,7 @@ import pandas as pd
 #%%
 peak_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA/MACS/peaks/RADICL_DNA_tot_peaks.bed"
 DNA_read_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/raw/DNA/RADICL_iPSC_DNA.bed"
-peak_read_inter_tbl_out_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA_peak_analysis/peak_read_inter_tbl.tsv"
+peak_read_inter_tbl_out_file = "/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA_peak_analysis/peak_DNA_read_inter_tbl.tsv"
 peak_df = pd.read_csv(peak_file,header=None,delimiter="\t")
 DNA_reads_tbl = pd.read_csv(DNA_read_file,header=None,delimiter="\t")
 
@@ -29,4 +29,3 @@ peak_read_inter_tbl.to_csv(peak_read_inter_tbl_out_file,
                            sep="\t",
                            header=True,
                            index=False)
-# %%
