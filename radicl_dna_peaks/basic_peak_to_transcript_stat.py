805127c651ee02aa3915bbe05a8683e90d51165d
princeps091-binf <princeps091@gmail.com>
2023-03-27 22:14:58 +0200

    simple summary stats

--- radicl_dna_peaks/basic_peak_to_transcript_stat.py
+++ radicl_dna_peaks/basic_peak_to_transcript_stat.py
@@ -17,6 +17,19 @@ peak_summary_df = (peak_to_transcript_df
  .reset_index()
  .assign(width=lambda df_:df_.end_peak-df_.start_peak)
  .sort_values('intra_prop'))
+
+#%%
+alt.data_transformers.disable_max_rows()
+
+(alt.Chart(peak_summary_df)
+.transform_density(
+    'intra_prop',
+    as_=['intra_prop', 'density'])
+.mark_line()
+.encode(
+    alt.X("intra_prop:Q"),
+    y='density:Q'))
+
 # %%
 alt.data_transformers.disable_max_rows()
 
@@ -39,10 +52,77 @@ alt.data_transformers.disable_max_rows()
 # %%
 peak_transcript_summary_df = (peak_to_transcript_df
  .assign(intra=lambda df_:df_.chrom_peak == df_.chrom_transcript)
- .groupby(['chrom_peak','start_peak','end_peak','ID'])
- .agg(read_count=('read_ID','count'))
+ .groupby(['chrom_peak','start_peak','end_peak'])
+ .agg(intra_prop=('intra','mean'),
+      gene_io=('genic',lambda df_: df_.isin(['genic']).mean()),
+      read_count=('read_ID','count'))
  .reset_index()
  .assign(width=lambda df_:df_.end_peak-df_.start_peak)
+ .assign(read_dens=lambda df_: df_.read_count/df_.width)
  .sort_values('read_count'))
 
+#%%
+(alt.Chart(peak_transcript_summary_df.assign(log_size=lambda df_:np.log10(df_.read_dens)))
+.transform_density(
+    'intra_prop',
+    as_=['intra_prop', 'density'])
+.mark_line()
+.encode(
+    alt.X("intra_prop:Q"),
+    y='density:Q'))
+
+
+#%%
+(alt.Chart(peak_transcript_summary_df.assign(log_size=lambda df_:np.log10(df_.width)))
+.mark_point(size=0.1,opacity=0.1)
+.encode(
+    alt.X("gene_io:Q"),
+    alt.Y('log_size:Q')))
+
+# %%
+peak_transcript_summary_df = (peak_to_transcript_df
+                              .assign(target_ID=lambda df_:df_.ID
+                                      .mask(df_.ID.isnull(),'intergenic'))
+                              .groupby(['chrom_peak','start_peak','end_peak','target_ID'])
+                              .agg(read_count=('read_ID','count'))
+                              .reset_index()
+                              .sort_values('read_count')
+                              .assign(width=lambda df_:df_.end_peak-df_.start_peak))
+
+#%%
+peak_summary = (peak_to_transcript_df
+ .loc[:,['chrom_peak','start_peak','end_peak','read_ID','genic']]
+ .drop_duplicates()
+ .groupby(['chrom_peak','start_peak','end_peak'])
+ .agg(gene_io=('genic',lambda df_: df_.isin(['genic']).mean()),
+      peak_nread=('read_ID','size'))
+ .reset_index())
+
+#%%
+(peak_transcript_summary_df
+ .merge(peak_summary)
+ .assign(read_prop=lambda df_:df_.read_count/df_.peak_nread))
+#%%
+(peak_transcript_summary_df
+ .merge(peak_summary)
+ .assign(read_prop=lambda df_:df_.read_count/df_.peak_nread)
+ .groupby('target_ID')
+ .agg(min=('read_count',min),
+      max=('read_count',max),
+      mean=('read_count','median'),
+      minp=('read_prop',min),
+      maxp=('read_prop',max),
+      meanp=('read_prop','mean'),
+      npeak=('chrom_peak','count'))
+  .query("npeak > 1")
+  .sort_values('meanp'))
+# %%
+(peak_transcript_summary_df
+ .groupby('target_ID')
+ .agg(min=('read_count',min),
+      max=('read_count',max),
+      mean=('read_count','median'),
+      npeak=('chrom_peak','count'))
+ .query("npeak < 1_000")
+ .sort_values('npeak'))
 # %%
--- radicl_dna_peaks/build_peak_to_transcript_map_tbl.py
+++ radicl_dna_peaks/build_peak_to_transcript_map_tbl.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
-import bioframe as bf
+import numpy as np
+import altair as alt
 
 #%%
 peak_to_read_file="/home/vipink/Documents/FANTOM6/data/RADICL_data/result/DNA_peak_analysis/peak_DNA_read_inter_tbl.tsv"
@@ -11,14 +12,19 @@ peak_to_read_df=pd.read_csv(peak_to_read_file,sep='\t')
 transcript_to_read_df=pd.read_csv(transcript_to_read_file,sep='\t')
 # %%
 peak_to_transcript_df=(peak_to_read_df
- .merge(transcript_to_read_df,how='inner',left_on='read_ID',right_on='read_ID',suffixes=('_peak','_transcript')))
+ .merge(transcript_to_read_df,
+        how='left',on='read_ID',
+        suffixes=('_peak','_transcript')))
+
+#%%
+peak_to_transcript_df = (peak_to_transcript_df
+ .assign(genic=lambda df_:(df_.strand
+                        .where(df_.strand.isnull(),'genic')
+                        .mask(df_.strand.isnull(),'intergenic'))))
 
 #%%
 (peak_to_transcript_df
  .to_csv(peak_to_transcript_file,
          sep='\t',
          index=False))
-# %%
-(peak_to_transcript_df
- .query('chrom_peak == chrom_transcript'))
-# %%
+
